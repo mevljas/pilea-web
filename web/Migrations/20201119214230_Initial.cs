7549fde151626dd6224c5fbbf72b18000c7a5d19
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-19 23:34:14 +0100

    Add migrations.

---
+++ web/Migrations/20201119214230_Initial.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace web.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Type",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlantCategory = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Type", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: true),
+                    Nickname = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Waters",
+                columns: table => new
+                {
+                    WatersID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Waters", x => x.WatersID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friend",
+                columns: table => new
+                {
+                    FriendID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friend", x => x.FriendID);
+                    table.ForeignKey(
+                        name: "FK_Friend_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    LocationID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.LocationID);
+                    table.ForeignKey(
+                        name: "FK_Location_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plant",
+                columns: table => new
+                {
+                    PlantID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Note = table.Column<string>(nullable: true),
+                    image = table.Column<byte[]>(nullable: true),
+                    DaysBetweenWatering = table.Column<int>(nullable: false),
+                    LastWatered = table.Column<DateTime>(nullable: false),
+                    UserID = table.Column<int>(nullable: false),
+                    CategoryID = table.Column<int>(nullable: false),
+                    LocationID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plant", x => x.PlantID);
+                    table.ForeignKey(
+                        name: "FK_Plant_Type_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Type",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Plant_Location_LocationID",
+                        column: x => x.LocationID,
+                        principalTable: "Location",
+                        principalColumn: "LocationID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Plant_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friend_UserID",
+                table: "Friend",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_UserID",
+                table: "Location",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plant_CategoryID",
+                table: "Plant",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plant_LocationID",
+                table: "Plant",
+                column: "LocationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plant_UserID",
+                table: "Plant",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friend");
+
+            migrationBuilder.DropTable(
+                name: "Plant");
+
+            migrationBuilder.DropTable(
+                name: "Waters");
+
+            migrationBuilder.DropTable(
+                name: "Type");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
