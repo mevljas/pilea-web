e2b6b5d083dc32f1965603cfd963c9644758d8d3
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-19 23:35:37 +0100

    Add project properties.

--- web/Startup.cs
+++ web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using web.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace web
 {
@@ -24,6 +26,10 @@ namespace web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Register PileaContext as a service
+            services.AddDbContext<PileaContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("PileaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
