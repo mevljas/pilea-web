5652cac22a6546987e9498486c7b463d72c4e41c
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-08 16:14:47 +0100

    generated APIs.

---
+++ web/Controllers/Api/FriendshipsApiController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/Friendship")]
+    [ApiController]
+    public class FriendshipsApiController : ControllerBase
+    {
+        private readonly PileaContext _context;
+
+        public FriendshipsApiController(PileaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FriendshipsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Friendship>>> GetFriendships()
+        {
+            return await _context.Friendships.ToListAsync();
+        }
+
+        // GET: api/FriendshipsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Friendship>> GetFriendship(string id)
+        {
+            var friendship = await _context.Friendships.FindAsync(id);
+
+            if (friendship == null)
+            {
+                return NotFound();
+            }
+
+            return friendship;
+        }
+
+        // PUT: api/FriendshipsApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFriendship(string id, Friendship friendship)
+        {
+            if (id != friendship.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(friendship).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FriendshipExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FriendshipsApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Friendship>> PostFriendship(Friendship friendship)
+        {
+            _context.Friendships.Add(friendship);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (FriendshipExists(friendship.UserId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetFriendship", new { id = friendship.UserId }, friendship);
+        }
+
+        // DELETE: api/FriendshipsApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Friendship>> DeleteFriendship(string id)
+        {
+            var friendship = await _context.Friendships.FindAsync(id);
+            if (friendship == null)
+            {
+                return NotFound();
+            }
+
+            _context.Friendships.Remove(friendship);
+            await _context.SaveChangesAsync();
+
+            return friendship;
+        }
+
+        private bool FriendshipExists(string id)
+        {
+            return _context.Friendships.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ web/Controllers/Api/PlantsApiController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/Plant")]
+    [ApiController]
+    public class PlantsApiController : ControllerBase
+    {
+        private readonly PileaContext _context;
+
+        public PlantsApiController(PileaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PlantsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
+        {
+            return await _context.Plants.ToListAsync();
+        }
+
+        // GET: api/PlantsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plant>> GetPlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            return plant;
+        }
+
+        // PUT: api/PlantsApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlant(int id, Plant plant)
+        {
+            if (id != plant.PlantID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PlantsApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Plant>> PostPlant(Plant plant)
+        {
+            _context.Plants.Add(plant);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlant", new { id = plant.PlantID }, plant);
+        }
+
+        // DELETE: api/PlantsApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Plant>> DeletePlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plants.Remove(plant);
+            await _context.SaveChangesAsync();
+
+            return plant;
+        }
+
+        private bool PlantExists(int id)
+        {
+            return _context.Plants.Any(e => e.PlantID == id);
+        }
+    }
+}
