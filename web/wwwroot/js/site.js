691c4b8e60b28e93b000f25edc916cf84376e287
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-27 11:19:42 +0100

    Enhancement/limit watering date attributes (#22)
    * Limit last watered date input range.

--- web/wwwroot/js/site.js
+++ web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// Limit last watered date input range. Set max date to today.
+
+
+
