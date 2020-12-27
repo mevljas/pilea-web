3c9bb3ab97c2b0b4c542e82dbe9f22671529dd4a
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-27 18:49:05 +0100

    API authorization enabled.

--- web/Controllers/Api/LocationsApiController.cs
+++ web/Controllers/Api/LocationsApiController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web.Data;
 using web.Models;
+using web.Filters;
 
 namespace web.Controllers_Api
 {
     [Route("api/v1/location")]
     [ApiController]
+    [ApiKeyAuth]
     public class LocationsApiController : ControllerBase
     {
         private readonly PileaContext _context;
--- web/Controllers/Api/PlantsApiController.cs
+++ web/Controllers/Api/PlantsApiController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web.Data;
 using web.Models;
+using web.Filters;
 
 namespace web.Controllers_Api
 {
     [Route("api/v1/Plant")]
     [ApiController]
+    [ApiKeyAuth]
     public class PlantsApiController : ControllerBase
     {
         private readonly PileaContext _context;
